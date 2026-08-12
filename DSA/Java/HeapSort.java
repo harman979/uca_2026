9cae257311d1d28b2fc54cb528468c4896334dde
Harmanjit Singh <harman221006@gmail.com>
2026-08-12 06:56:24 +0000

    Implement Heap Sort using max heap

---
+++ DSA/Java/HeapSort.java
@@ -0,0 +1,44 @@
+class HeapSort {
+
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = n / 2; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void heapify(int[] arr, int n, int i) {
+        while (true) {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && arr[left] > arr[largest]) {
+                largest = left;
+            }
+
+            if (right < n && arr[right] > arr[largest]) {
+                largest = right;
+            }
+
+            if (largest == i) {
+                break;
+            }
+
+            swap(arr, i, largest);
+            i = largest;
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
