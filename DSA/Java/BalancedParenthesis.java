0af16e3f14514dec10c0756195c13611f9189100
Harmanjit Singh <harman221006@gmail.com>
2026-08-24 18:41:14 +0000

    Add balanced parenthesis solution

---
+++ DSA/Java/BalancedParenthesis.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class BalancedParenthesis {
+
+    public static boolean isBalanced(String expression) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : expression.toCharArray()) {
+
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            }
+
+            else if (ch == ')' || ch == ']' || ch == '}') {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == ']' && top != '[') ||
+                    (ch == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isBalanced("[()]{}"));        // true
+        System.out.println(isBalanced("[()"));          // false
+    }
+}
